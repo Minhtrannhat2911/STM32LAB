b1b8f1ffae0bb919d4ef8fa3f81241fdc861808a
DESKTOP-6NRT369\Dell <minh.trannhat2911@hcmut.edu.vn>
2024-09-17 15:36:04 +0700

    Change Main

--- lab1.0/Core/Inc/main.h
+++ lab1.0/Core/Inc/main.h
@@ -58,6 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SEG1_0_Pin GPIO_PIN_1
+#define SEG1_0_GPIO_Port GPIOA
+#define SEG1_1_Pin GPIO_PIN_2
+#define SEG1_1_GPIO_Port GPIOA
+#define SEG1_2_Pin GPIO_PIN_3
+#define SEG1_2_GPIO_Port GPIOA
 #define LED_GREEN_Pin GPIO_PIN_4
 #define LED_GREEN_GPIO_Port GPIOA
 #define LED_RED_Pin GPIO_PIN_5
@@ -76,6 +82,14 @@ void Error_Handler(void);
 #define LED_11_GPIO_Port GPIOB
 #define LED_12_Pin GPIO_PIN_11
 #define LED_12_GPIO_Port GPIOB
+#define SEG1_3_Pin GPIO_PIN_12
+#define SEG1_3_GPIO_Port GPIOB
+#define SEG1_4_Pin GPIO_PIN_13
+#define SEG1_4_GPIO_Port GPIOB
+#define SEG1_5_Pin GPIO_PIN_14
+#define SEG1_5_GPIO_Port GPIOB
+#define SEG1_6_Pin GPIO_PIN_15
+#define SEG1_6_GPIO_Port GPIOB
 #define SEG_1_Pin GPIO_PIN_8
 #define SEG_1_GPIO_Port GPIOA
 #define SEG_2_Pin GPIO_PIN_9
