2cc5067e6f4335cfa927bb072eb596062d654a86
DESKTOP-6NRT369\Dell <minh.trannhat2911@hcmut.edu.vn>
2024-09-17 15:07:53 +0700

    EX2

--- lab1.0/Core/Src/main.c
+++ lab1.0/Core/Src/main.c
@@ -93,6 +93,18 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_WritePin(LED_RED_GPIO_Port , LED_RED_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port , LED_YELLOW_Pin, SET);
+	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port , LED_GREEN_Pin, SET);
+	  HAL_Delay(5000);
+	  HAL_GPIO_WritePin(LED_RED_GPIO_Port , LED_RED_Pin, SET);
+	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port , LED_YELLOW_Pin, RESET);
+	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port , LED_GREEN_Pin, SET);
+	  HAL_Delay(2000);
+	  HAL_GPIO_WritePin(LED_RED_GPIO_Port , LED_RED_Pin, SET);
+	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port , LED_YELLOW_Pin, SET);
+	  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port , LED_GREEN_Pin, RESET);
+	  HAL_Delay(3000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
